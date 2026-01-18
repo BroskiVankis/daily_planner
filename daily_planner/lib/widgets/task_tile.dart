71058369485ea5872ba0c7b81e07c21c7a8e2ecb
Ivan <ivanov.vankis@gmail.com>
2026-01-18 17:38:37 +0200

    Small animation on completion

--- daily_planner/lib/widgets/task_tile.dart
+++ daily_planner/lib/widgets/task_tile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/Task.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
     required this.index,
@@ -16,7 +16,27 @@ class TaskTile extends StatelessWidget {
   final VoidCallback onDelete;
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool _completed = false;
+
+  @override
+  void didUpdateWidget(covariant TaskTile oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.task.isDone && !oldWidget.task.isDone) {
+      setState(() => _completed = true);
+      Future.delayed(const Duration(milliseconds: 600), () {
+        if (mounted) setState(() => _completed = false);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final index = widget.index;
+    final task = widget.task;
     final isEven = index % 2 == 0;
     final bgColor = isEven
         ? const Color.fromARGB(255, 176, 224, 246)
@@ -36,61 +56,72 @@ class TaskTile extends StatelessWidget {
             child: const Icon(Icons.delete, color: Colors.white),
           ),
         ),
-        onDismissed: (_) => onDelete(),
+        onDismissed: (_) => widget.onDelete(),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(16),
-          child: Container(
-            decoration: BoxDecoration(
-              color: bgColor,
-              borderRadius: BorderRadius.circular(16),
-            ),
-            padding: const EdgeInsets.all(12),
-            child: Row(
-              children: [
-                // Circle with incrementing numbers
-                Container(
-                  width: 36,
-                  height: 36,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.blueAccent,
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(
-                    '${index + 1}',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+          child: AnimatedOpacity(
+            opacity: _completed ? 0.6 : 1.0,
+            duration: const Duration(milliseconds: 300),
+            child: AnimatedScale(
+              scale: _completed ? 0.95 : 1.0,
+              duration: const Duration(milliseconds: 300),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: bgColor,
+                  borderRadius: BorderRadius.circular(16),
                 ),
-                const SizedBox(width: 12),
-                // Task title + subtitle
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        task.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          decoration: task.isDone
-                              ? TextDecoration.lineThrough
-                              : TextDecoration.none,
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  children: [
+                    // Circle with incrementing numbers
+                    Container(
+                      width: 36,
+                      height: 36,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blueAccent,
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        '${index + 1}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      const SizedBox(height: 4),
-                      Text(
-                        task.isDone ? 'Completed' : 'Not Done Yet',
-                        style: TextStyle(fontSize: 13, color: Colors.grey[700]),
+                    ),
+                    const SizedBox(width: 12),
+                    // Task title + subtitle
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            task.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              decoration: task.isDone
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            task.isDone ? 'Completed' : 'Not Done Yet',
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                    // Checkbox
+                    Checkbox(value: task.isDone, onChanged: widget.onChanged),
+                  ],
                 ),
-                // Checkbox
-                Checkbox(value: task.isDone, onChanged: onChanged),
-              ],
+              ),
             ),
           ),
         ),
