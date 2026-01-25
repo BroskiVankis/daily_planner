6623efd0a796a3f892bf58b0926047e4caa7a4af
Ivan <ivanov.vankis@gmail.com>
2026-01-25 10:51:44 +0200

    IV-008: Day Calender View

--- daily_planner/test/widget_test.dart
+++ daily_planner/test/widget_test.dart
@@ -11,20 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:daily_planner/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Landing page renders', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DailyPlannerApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the landing page headline is shown.
+    expect(find.text('Want your days to be in order?'), findsOneWidget);
   });
 }
